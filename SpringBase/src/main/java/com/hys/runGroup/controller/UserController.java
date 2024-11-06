428f75eadd7dc6f209c278e973113747c2f9df6f
BRXYM <BRXYM@outlook.com>
2024-11-06 16:11:05 +0800

    更改controller

--- SpringBase/src/main/java/com/hys/runGroup/controller/UserController.java
+++ SpringBase/src/main/java/com/hys/runGroup/controller/UserController.java
@@ -4,12 +4,14 @@ package com.hys.runGroup.controller;
 import com.hys.runGroup.domain.User;
 import com.hys.runGroup.service.UserService;
 import com.hys.runGroup.utils.Result;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Resource
     private UserService userService;
 
     @GetMapping("/getAllUsers")
